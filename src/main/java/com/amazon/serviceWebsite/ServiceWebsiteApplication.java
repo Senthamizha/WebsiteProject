38e9c85832ee8a465fab5159d6c5ea5e4361ccab
Senthamizha <aanand.a4@gmail.com>
2021-08-17 19:45:18 +0530

    Amazon Project Creation Commit

---
+++ src/main/java/com/amazon/serviceWebsite/ServiceWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.serviceWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amazon/serviceWebsite/ServiceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.serviceWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
